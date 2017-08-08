aa96c4bdf056312998c801b06ea96d11f01295c8
Daniel Kuck-Alvarez <dkuck@thatsus.com>
2017-08-08 14:29:26 -0400

    Bring in easeljs

---
+++ resources/assets/js/easel.js
@@ -0,0 +1,3 @@
+require('../../../node_modules/yuki-createjs/lib/easeljs-0.8.2.combined.js');
+
+module.exports = createjs;
--- test/EaselCanvas.spec.js
+++ test/EaselCanvas.spec.js
@@ -4,24 +4,22 @@ import EaselCanvas from '../resources/assets/js/components/EaselCanvas.vue';
 import $ from 'jquery';
 
 describe('EaselCanvas', function () {
-    describe('#instantiate', function () {
-        var vm = new Vue({
-            template: '<easel-canvas ref="easelCanvas"><span id="im-in-a-slot"></span></easel-canvas>',
-            components: {
-                'easel-canvas': EaselCanvas,
-            },
-        }).$mount();
+    var vm = new Vue({
+        template: '<easel-canvas ref="easelCanvas"><span id="im-in-a-slot"></span></easel-canvas>',
+        components: {
+            'easel-canvas': EaselCanvas,
+        },
+    }).$mount();
 
-        it('should have a canvas object', function () {
-            assert($(vm.$el).find('canvas').length === 1);
-        });
+    it('should have a canvas object', function () {
+        assert($(vm.$el).find('canvas').length === 1);
+    });
 
-        it('should have a canvas object referenced as `easel`', function () {
-            assert(vm.$refs.easelCanvas.$refs.easel !== null);
-        });
+    it('should have a canvas object referenced as `easel`', function () {
+        assert(vm.$refs.easelCanvas.$refs.easel);
+    });
 
-        it('should have the slot stuff we put in', function () {
-            assert($(vm.$el).find('#im-in-a-slot') !== null);
-        });
+    it('should have the slot stuff we put in', function () {
+        assert($(vm.$el).find('#im-in-a-slot'));
     });
 });
