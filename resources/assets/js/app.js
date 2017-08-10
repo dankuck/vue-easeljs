2bc4590ef8987eb43d228d087c2099d6ce3590e2
dankuck <dankuck@gmail.com>
2017-08-09 23:59:46 -0400

    SpriteSheet works live too.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,6 +2,7 @@ require('vue-resource');
 
 Vue.component('easel-canvas', require('./components/EaselCanvas.vue'));
 Vue.component('easel-shape', require('./components/EaselShape.vue'));
+Vue.component('easel-sprite-sheet', require('./components/EaselSpriteSheet.vue'));
 
 const app = new Vue({
     el: '#app'
