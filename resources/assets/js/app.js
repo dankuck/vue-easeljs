cf8dc0e08c417f32c4bc38fee984b79b1945b327
Daniel Kuck-Alvarez <dkuck@thatsus.com>
2017-08-11 16:28:29 -0400

    Container is now usable.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,6 +5,7 @@ Vue.component('easel-shape', require('./components/EaselShape.vue'));
 Vue.component('easel-sprite-sheet', require('./components/EaselSpriteSheet.vue'));
 Vue.component('easel-sprite', require('./components/EaselSprite.vue'));
 Vue.component('easel-bitmap', require('./components/EaselBitmap.vue'));
+Vue.component('easel-container', require('./components/EaselContainer.vue'));
 
 const app = new Vue({
     el: '#app',
