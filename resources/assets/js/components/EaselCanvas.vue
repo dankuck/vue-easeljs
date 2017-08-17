95e8aeecf3c7978c156eb7ad4a1e977149439877
Daniel Kuck-Alvarez <dkuck@thatsus.com>
2017-08-17 13:15:05 -0400

    Turn anti-aliasing off

--- resources/assets/js/components/EaselCanvas.vue
+++ resources/assets/js/components/EaselCanvas.vue
@@ -5,25 +5,36 @@
 </template>
 
 <script>
-import easeljs from '../easel.js';
 import EaselEventBinder from '../EaselEventBinder.js';
+import easeljs from '../easel.js';
 import EaselParent from '../mixins/EaselParent.js';
 
 export default {
     mixins: [EaselParent],
+    props: ['antiAlias'],
     data() {
         return {
             component: null,
+            context: null,
         };
     },
     destroyed() {
         easeljs.Touch.disable(this.component);
     },
+    watch: {
+        antiAlias() {
+            this.context.imageSmoothingEnabled = this.antiAlias;
+        },
+    },
     mounted() {
         this.component = new easeljs.Stage(this.$refs.easel);
+        this.context = this.component.canvas.getContext('2d');
         easeljs.Touch.enable(this.component, false, true);
         EaselEventBinder.bindEvents(this, this.component);
         easeljs.Ticker.addEventListener("tick", (event) => this.component.update(event));
+        if (typeof this.antiAlias !== 'undefined') {
+            this.context.imageSmoothingEnabled = this.antiAlias;
+        }
         this.addPendingChildren();
     },
 };
