ba884293ba043d59886adb87753fa7e8e1717bc3
dankuck <dankuck@gmail.com>
2017-08-25 00:09:51 -0400

    more examlpes

--- src/Example4.vue
+++ src/Example4.vue
@@ -3,23 +3,24 @@
         <pre>{{ html }}</pre>
 
         <easel-canvas width="500" height="100">
-            <easel-shape
-                form="rect"
-                :dimensions="[500,100]"
-                :x="250"
-                :y="50"
-                fill="#CCCCFF"
+            <easel-container
+                :flip="horizontal"
+                :shadow="['white',0,0,7]"
             >
-            </easel-shape>
-            <easel-text 
-                text="This is so easy!"
-                :x="250"
-                :y="75"
-                font="70px Calibri"
-                color="white"
-                :shadow="['#000088',3,2,3]"
-            >
-            </easel-text>
+                <easel-bitmap
+                    :x="250"
+                    :y="50"
+                    image="/images/wooden-sign-texture.png"
+                >
+                </easel-bitmap>
+                <easel-text 
+                    text="Dan's Left Shoe Emporium"
+                    font="50px 'Times New Roman'"
+                    :x="250"
+                    :y="75"
+                >
+                </easel-text>
+            </easel-container>
         </easel-canvas>
     </div>
 </template>
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ Vue.component('gary', require('./Gary.vue'));
 Vue.component('example1', require('./Example1.vue'));
 Vue.component('example2', require('./Example2.vue'));
 Vue.component('example3', require('./Example3.vue'));
+Vue.component('example4', require('./Example4.vue'));
 
 var app = new Vue({
     el: '#app',
