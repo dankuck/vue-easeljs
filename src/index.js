9dbf3dec62f49c51f290cf3e219ded117fef8be7
Dan Kuck <dan@honeyfund.com>
2019-08-21 00:04:20 -0400

    Make parts accessible
    For folks who want to play around in devtools, or really any reason

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 module.exports = {
+    createjs:         require('../easeljs/easel.js'),
+    easeljs:          require('../easeljs/easel.js'),
+    EaselBitmap:      require('./components/EaselBitmap.vue'),
+    EaselCanvas:      require('./components/EaselCanvas.vue'),
+    EaselContainer:   require('./components/EaselContainer.vue'),
+    EaselShape:       require('./components/EaselShape.vue'),
+    EaselSprite:      require('./components/EaselSprite.vue'),
+    EaselSpriteSheet: require('./components/EaselSpriteSheet.vue'),
+    EaselText:        require('./components/EaselText.vue'),
     install(Vue) {
-        Vue.component('easel-bitmap', require('./components/EaselBitmap.vue'));
-        Vue.component('easel-canvas', require('./components/EaselCanvas.vue'));
-        Vue.component('easel-container', require('./components/EaselContainer.vue'));
-        Vue.component('easel-shape', require('./components/EaselShape.vue'));
-        Vue.component('easel-sprite', require('./components/EaselSprite.vue'));
-        Vue.component('easel-sprite-sheet', require('./components/EaselSpriteSheet.vue'));
-        Vue.component('easel-text', require('./components/EaselText.vue'));
+        Vue.component('easel-bitmap', this.EaselBitmap);
+        Vue.component('easel-canvas', this.EaselCanvas);
+        Vue.component('easel-container', this.EaselContainer);
+        Vue.component('easel-shape', this.EaselShape);
+        Vue.component('easel-sprite', this.EaselSprite);
+        Vue.component('easel-sprite-sheet', this.EaselSpriteSheet);
+        Vue.component('easel-text', this.EaselText);
     }
 };
