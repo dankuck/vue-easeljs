5bcbd6909bbc0dfb627678e7c9f7e228e5924e50
Dan Kuck <dan@honeyfund.com>
2019-10-14 00:04:07 -0400

    Filters

--- src/filters/PixelStrokeFilter.js
+++ src/filters/PixelStrokeFilter.js
@@ -6,13 +6,18 @@ import easeljs from '../../easeljs/easel.js';
  |------------------------
  | Add a stroke to an element
  */
+
+const calculatedBrushes = {};
+
 export default class PixelStrokeFilter {
 
-    constructor(stroke = []) {
+    constructor(stroke = [], size = 1) {
         this.strokeRed = stroke[0] || 0;
         this.strokeGreen = stroke[1] || 0;
         this.strokeBlue = stroke[2] || 0;
         this.strokeAlpha = stroke[3] || 255;
+        this.brush = this.calculateBrush(size);
+        this.alphaCache = {};
     }
 
     adjustImageData(imageData) {
@@ -20,40 +25,112 @@ export default class PixelStrokeFilter {
         const {data, width, height} = imageData;
         const length = data.length;
         const copy = data.slice(0);
-        const activatePixel = (x, y) => {
+        const activatePixel = (x, y, a) => {
             if (x < 0 || y < 0 || x >= width || y >= height) {
                 return;
             }
             const i = (y * width + x) * 4;
-            if (!data[i + 3]) {
+            const alpha = this.alphaCache[a] || (this.alphaCache[a] = Math.floor(strokeAlpha * a));
+            if (!copy[i + 3] && data[i + 3] < alpha) {
                  data[i + 0] = strokeRed;
                  data[i + 1] = strokeGreen;
                  data[i + 2] = strokeBlue;
-                 data[i + 3] = strokeAlpha;
+                 data[i + 3] = alpha;
             }
         };
+        const applyBrush = (sx, sy) => {
+            for (let i = 0; i < this.brush.length; i++) {
+                const {y, minX, maxX, a} = this.brush[i];
+                activatePixel(sx + minX, sy + y, a);
+                activatePixel(sx + maxX, sy + y, a);
+                for (let j = minX + 1; j <= maxX - 1; j++) {
+                    activatePixel(sx + j, sy + y, 1);
+                }
+            }
+        }
         for (let x = 0; x < width; x++) {
             for (let y = 0; y < height; y++) {
                 if (copy[(y * width + x) * 4 + 3] > 0) {
-                    const up = y - 1;
-                    const down = y + 1;
-                    const left = x - 1;
-                    const right = x + 1;
-
-                    activatePixel(left, up);
-                    activatePixel(x, up);
-                    activatePixel(right, up);
-
-                    activatePixel(left, y);
-                    // activatePixel(x, y); // already set!
-                    activatePixel(right, y);
-
-                    activatePixel(left, down);
-                    activatePixel(x, down);
-                    activatePixel(right, down);
+                    applyBrush(x, y);
                 }
             }
         }
         return true;
     }
+
+    calculateBrush(size) {
+        if (calculatedBrushes[size]) {
+            return calculatedBrushes[size];
+        }
+        /*
+        Imagine a circle like this
+               1 | 2
+             \       /
+           8           3
+           -           -
+           7           4
+             /       \
+               6 | 5
+        */
+        const map = {};
+        // Figure out sector 4
+        for (let y = 0; y <= size; y++) {
+            const tan = y / size;
+            const angle = Math.atan(tan);
+            const cos = Math.cos(angle);
+            const sin = Math.sin(angle);
+            const realX = cos * size;
+            const realY = sin * size;
+            const ceilX = Math.ceil(realX);
+            const ceilY = Math.ceil(realY);
+            const intensityX = realX - Math.floor(realX);
+            const intensityY = realY - Math.floor(realY);
+            let intensity = (intensityX + intensityY) / 2;
+            if (intensity === 0) {
+                intensity = 1;
+            }
+            map[ceilY] = {
+                x: ceilX,
+                y: ceilY,
+                a: intensity,
+            };
+        }
+        // Flip sector 4's x and y to get sector 5
+        for (let field in map) {
+            const {x: y, y: x, a} = map[field];
+            if (!map[y]) {
+                map[y] = {
+                    x,
+                    y,
+                    a,
+                };
+            }
+        }
+        // Use that to build horizontal lines
+        // from 7 to 4
+        // from 6 to 5
+        // from 1 to 2
+        // from 8 to 3
+        const lines = [];
+        for (let field in map) {
+            const {x, y, a} = map[field];
+            lines.push({
+                y,
+                minX: -x,
+                maxX: x,
+                a,
+            });
+            if (y !== 0) {
+                lines.push({
+                    y: -y,
+                    minX: -x,
+                    maxX: x,
+                    a,
+                });
+            }
+        }
+        calculatedBrushes[size] = lines;
+        return calculatedBrushes[size];
+    }
 };
+
--- test/PixelStrokeFilter.spec.js
+++ test/PixelStrokeFilter.spec.js
@@ -105,4 +105,83 @@ describe('PixelStrokeFilter', function () {
             stringify(imageData.data)
         );
     });
+
+    it.only('calculates a size 1 circle', function () {
+        const filter = new PixelStrokeFilter([0, 0, 0, 1], 1);
+        const expected = {
+            '1': {y: 1, minX: -1, maxX: 1, a: Math.sin(Math.PI / 4)},
+            '0': {y: 0, minX: -1, maxX: 1, a: 1},
+            '-1': {y: -1, minX: -1, maxX: 1, a: Math.sin(Math.PI / 4)},
+        };
+        equal(Object.keys(expected).length, filter.brush.length, JSON.stringify(filter.brush));
+        filter.brush.forEach(point => {
+            equal(expected[point.y], point, JSON.stringify(filter.brush));
+        });
+    });
+
+    it.only('calculates a size 2 circle', function () {
+        const filter = new PixelStrokeFilter([0, 0, 0, 1], 2);
+        const expected = {
+            '-2': {"y":-2, "minX":-2, "maxX":2, "a":0.41421356237309503},
+            '-1': {"y":-1, "minX":-2, "maxX":2, "a":0.8416407864998738},
+            '0':  {"y":0, "minX":-2, "maxX":2, "a":1},
+            '1':  {"y":1, "minX":-2, "maxX":2, "a":0.8416407864998738},
+            '2':  {"y":2, "minX":-2, "maxX":2, "a":0.41421356237309503},
+        };
+        equal(Object.keys(expected).length, filter.brush.length, JSON.stringify(filter.brush));
+        filter.brush.forEach(point => {
+            equal(expected[point.y], point, JSON.stringify(filter.brush));
+        });
+    });
+
+    it.only('calculates a size 20 circle', function () {
+        const filter = new PixelStrokeFilter([0, 0, 0, 1], 20);
+        const expected = {
+            '-20': {"y":-20, "minX":0, "maxX":0, "a":1},
+            '-19': {"y":-19, "minX":-7, "maxX":7, "a":0.7420878109412348},
+            '-18': {"y":-18, "minX":-9, "maxX":9, "a":0.9164078649987388},
+            '-17': {"y":-17, "minX":-11, "maxX":11, "a":0.8343196689605117},
+            '-16': {"y":-16, "minX":-12, "maxX":12, "a":1},
+            '-15': {"y":-15, "minX":-15, "maxX":15, "a":0.14213562373095012},
+            '-14': {"y":-14, "minX":-15, "maxX":15, "a":0.6374889550910687},
+            '-13': {"y":-13, "minX":-16, "maxX":16, "a":0.5958773785499982},
+            '-12': {"y":-12, "minX":-16, "maxX":16, "a":1},
+            '-11': {"y":-11, "minX":-17, "maxX":17, "a":0.8343196689605117},
+            '-10': {"y":-10, "minX":-18, "maxX":18, "a":0.5813465844880294},
+            '-9':  {"y":-9, "minX":-18, "maxX":18, "a":0.9164078649987388},
+            '-8':  {"y":-8, "minX":-19, "maxX":19, "a":0.4986736723936298},
+            '-7':  {"y":-7, "minX":-19, "maxX":19, "a":0.7420878109412348},
+            '-6':  {"y":-6, "minX":-20, "maxX":20, "a":0.4517417078749686},
+            '-5':  {"y":-5, "minX":-20, "maxX":20, "a":0.6267812518166496},
+            '-4':  {"y":-4, "minX":-20, "maxX":20, "a":0.7669681082910411},
+            '-3':  {"y":-3, "minX":-20, "maxX":20, "a":0.8727680579854207},
+            '-2':  {"y":-2, "minX":-20, "maxX":20, "a":0.9454090923098815},
+            '-1':  {"y":-1, "minX":-20, "maxX":20, "a":0.9868995582173695},
+            '0':   {"y":0, "minX":-20, "maxX":20, "a":1},
+            '1':   {"y":1, "minX":-20, "maxX":20, "a":0.9868995582173695},
+            '2':   {"y":2, "minX":-20, "maxX":20, "a":0.9454090923098815},
+            '3':   {"y":3, "minX":-20, "maxX":20, "a":0.8727680579854207},
+            '4':   {"y":4, "minX":-20, "maxX":20, "a":0.7669681082910411},
+            '5':   {"y":5, "minX":-20, "maxX":20, "a":0.6267812518166496},
+            '6':   {"y":6, "minX":-20, "maxX":20, "a":0.4517417078749686},
+            '7':   {"y":7, "minX":-19, "maxX":19, "a":0.7420878109412348},
+            '8':   {"y":8, "minX":-19, "maxX":19, "a":0.4986736723936298},
+            '9':   {"y":9, "minX":-18, "maxX":18, "a":0.9164078649987388},
+            '10':  {"y":10, "minX":-18, "maxX":18, "a":0.5813465844880294},
+            '11':  {"y":11, "minX":-17, "maxX":17, "a":0.8343196689605117},
+            '12':  {"y":12, "minX":-16, "maxX":16, "a":1},
+            '13':  {"y":13, "minX":-16, "maxX":16, "a":0.5958773785499982},
+            '14':  {"y":14, "minX":-15, "maxX":15, "a":0.6374889550910687},
+            '15':  {"y":15, "minX":-15, "maxX":15, "a":0.14213562373095012},
+            '16':  {"y":16, "minX":-12, "maxX":12, "a":1},
+            '17':  {"y":17, "minX":-11, "maxX":11, "a":0.8343196689605117},
+            '18':  {"y":18, "minX":-9, "maxX":9, "a":0.9164078649987388},
+            '19':  {"y":19, "minX":-7, "maxX":7, "a":0.7420878109412348},
+            '20':  {"y":20, "minX":0, "maxX":0, "a":1},
+        };
+        equal(Object.keys(expected).length, filter.brush.length, JSON.stringify(filter.brush));
+        filter.brush.forEach(point => {
+            equal(expected[point.y], point, JSON.stringify(filter.brush));
+        });
+    });
 });
